fce0f80e29f765a0336bfb7f5339afacf09ff63c
Rgaherwar <114740471+RahulGaherwar6@users.noreply.github.com>
2024-08-29 14:31:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
 
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello......!");
-=======
-        System.out.println("Hello...!");
->>>>>>> caf17e2001f2bcfd204341a3a0a6877cbde257c6
+          System.out.println("Hello......!");
     }
 }
